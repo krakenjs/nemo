2ad58e29c9906044977fd1fa534378c9fc951fbb
Matt Edelman <medelman@paypal.com>
2018-02-06 18:37:12 -0800

    clean up some debugging code

--- test/nested.js
+++ test/nested.js
@@ -3,7 +3,7 @@ describe('@suite1@suite2@suite3@suite4@', function () {
   it('may fail a few times1', function () {
     let nemo = this.nemo;
     //verify nemo.mocha property
-    if (nemo.mocha === this) {
+    if (!nemo.mocha === this) {
       return Promise.reject(new Error('didnt find mocha context at nemo.mocha'));
     }
     return nemo.driver.get(nemo.data.baseUrl)
