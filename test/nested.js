9dad901e51bb638680107e7b82cafb2ee4709a48
Matt Edelman <medelman@paypal.com>
2018-06-08 13:58:30 -0700

    add nemo to mocha lifecycle context so its available to before/after/beforeEach/afterEach

--- test/nested.js
+++ test/nested.js
@@ -1,5 +1,29 @@
 
 describe('@suite1@suite2@suite3@suite4@', function () {
+  before(function () {
+    if (this.nemo) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error('no nemo'));
+  });
+  after(function () {
+    if (this.nemo) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error('no nemo'));
+  });
+  beforeEach(function () {
+    if (this.nemo) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error('no nemo'));
+  });
+  afterEach(function () {
+    if (this.nemo) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error('no nemo'));
+  });
   it('may fail a few times1', function () {
     let nemo = this.nemo;
     //verify nemo.mocha property
@@ -15,6 +39,30 @@ describe('@suite1@suite2@suite3@suite4@', function () {
       })
   });
   describe('@inner@', function () {
+    before(function () {
+      if (this.nemo) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('no nemo'));
+    });
+    after(function () {
+      if (this.nemo) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('no nemo'));
+    });
+    beforeEach(function () {
+      if (this.nemo) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('no nemo'));
+    });
+    afterEach(function () {
+      if (this.nemo) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('no nemo'));
+    });
     it('may fail a few times2', function () {
       let nemo = this.nemo;
       //verify nemo.mocha property
