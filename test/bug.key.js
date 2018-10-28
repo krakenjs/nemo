5d7ab87dd4d0455035fa50109de31798f638868f
Matt Edelman <medelman@paypal.com>
2018-10-28 08:14:33 -0700

    adding a test case to debug issue #32

---
+++ test/bug.key.js
@@ -0,0 +1,9 @@
+
+describe('@pay@', function () {
+  it('should show a signup form for payment processor', async function () {
+    let nemo = this.nemo;
+    debugger;
+    console.log("dataset:", nemo.data.dataset);
+    console.log(nemo.data);
+  });
+});
