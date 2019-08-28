5eb50ef0f0b4cb38163426971885cdc80d1c6e2b
Bijoy Vijayachandran <bijoyv@gmail.com>
2019-08-28 14:02:05 -0700

    added event for before test, before and after suite

--- test/config/listeners.js
+++ test/config/listeners.js
@@ -8,4 +8,19 @@ module.exports = [{
   listener: (context, event) => {
     console.log('custom:unittest event fired', event);
   }
+},{
+ type: 'suite',
+ listener: (context, event) => {
+   console.log('suite event fired', event.tags.uid);
+ }
+},{
+  type: 'beforesuite',
+    listener: (context, event) => {
+    console.log('before suite event fired', event.tags.uid);
+  }
+},{
+  type: 'beforetest',
+    listener: (context, event) => {
+    console.log('before test fired', event.test.title);
+  }
 }];
