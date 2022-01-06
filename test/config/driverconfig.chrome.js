b1ba94aed62950ca220e098e2d87b19549bd2777
Matt Edelman <medelman@paypal.com>
2022-01-06 13:57:33 -0800

    adding factory function module

---
+++ test/config/driverconfig.chrome.js
@@ -0,0 +1,18 @@
+const chrome = require('selenium-webdriver/chrome')
+const { Builder } = require('selenium-webdriver')
+function getConfig() {
+  const width = 640
+  const height = 480
+  return new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(
+      new chrome.Options().headless().windowSize({ width, height })
+    )
+}
+
+module.exports = function () {
+  return getConfig()
+    .build()
+};
+
+module.exports.getConfig = getConfig;
